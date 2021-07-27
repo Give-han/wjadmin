6d41d36124641ed8324a6918c289938114687bab
weizhou <zhu_tao1213@163.com>
2021-07-27 20:53:02 +0800

    init

---
+++ src/main/java/com/zhut/wjadmin/WjadminApplication.java
@@ -0,0 +1,13 @@
+package com.zhut.wjadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WjadminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WjadminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhut/wjadmin/WjadminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhut.wjadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WjadminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
